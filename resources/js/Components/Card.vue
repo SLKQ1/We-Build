edab4abab3a8a113c06c450b2af81802aa9ba7e5
faizqureshi <qureshi.a.faiz@gmail.com>
2022-11-18 19:21:44 -0700

    updated card so button sticks to bottom

--- resources/js/Components/Card.vue
+++ resources/js/Components/Card.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="max-w-xs p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 md:max-w-2xl">
+    <div class="flex flex-col max-w-xs p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 md:max-w-2xl">
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {{project.title}} </h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> {{description}}  </p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-400 rounded-lg hover:bg-indigo-600">
-            Check it out!
-            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-        </a>
+
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> {{description}}</p>
+
+        <div class="mt-auto">
+            <a @click="$emit('cardClicked', project.id)" href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-400 rounded-lg hover:bg-indigo-600">
+                Check it out!
+            </a>
+        </div>
     </div>
 </template>
 
