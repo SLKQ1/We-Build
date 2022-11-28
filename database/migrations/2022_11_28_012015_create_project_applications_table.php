26eac334585f111adbb2ded8fa513a65ee151b32
Faiz Qureshi <qureshi.a.faiz@gmail.com>
2022-11-27 18:51:25 -0700

    created migrations for tables needed

---
+++ database/migrations/2022_11_28_012015_create_project_applications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete(); 
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->string('application_name');
+            $table->longText('application_description');
+            $table->integer('status'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_applications');
+    }
+};
