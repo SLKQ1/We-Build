dc959ba1e0bdd17f09056ffc277c034468792619
Faiz Qureshi <qureshi.a.faiz@gmail.com>
2022-11-28 22:25:10 -0700

    split project view and project edit view

--- resources/js/Pages/Project.vue
+++ resources/js/Pages/Project.vue
@@ -1,26 +1,5 @@
 <template>
-  <AuthenticatedLayout v-if="$page.props.auth.user">
-    <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        Project X
-      </h2>
-    </template>
-    <div class="py-12">
-      <div class="mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-          <div class="p-6 bg-white border-b border-gray-200">
-            <div class="flex flex-col gap-y-3">
-              <TextInput id="name" type="text" placeholder="Enter project name here" class="mt-1 block w-full" required autofocus
-                autocomplete="name" />
-              <EditorVue />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </AuthenticatedLayout>
-
-  <GuestLayout v-else>
+  <GuestLayout>
     <div class="flex flex-col gap-y-3 items-center justify-between">
       <h1 class="font-semibold underline text-center text-5xl text-gray-800 py-4">Project X</h1>
       <div>
@@ -49,10 +28,5 @@
 </template>
 
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import EditorVue from '@/Components/Editor.vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import TextInput from '@/Components/TextInput.vue';
-
 </script>
