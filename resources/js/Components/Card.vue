a8d00e41f167eee7177c69e279f00db0aee879d9
Faiz Qureshi <qureshi.a.faiz@gmail.com>
2022-12-05 21:03:54 -0700

    fixed card and show compoents so that rich text displays correctly

--- resources/js/Components/Card.vue
+++ resources/js/Components/Card.vue
@@ -4,7 +4,7 @@
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {{project.title}} </h5>
         </a>
 
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> {{description}}</p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400" v-html="description"> </p>
 
         <div class="mt-auto">
             <Link :href="route('projects.show', {id: project.id})" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-400 rounded-lg hover:bg-indigo-600">
--- resources/js/Pages/Projects/Show.vue
+++ resources/js/Pages/Projects/Show.vue
@@ -1,17 +1,18 @@
 <template>
   <GuestLayout>
-    <div class="flex flex-col gap-y-3 items-center justify-between">
-      <h1 class="font-semibold underline text-center text-5xl text-gray-800 py-4"> {{project.title}} </h1>
-      <div>
-        <p>
-         {{project.description}}
-        </p>
+    <html lang="en" class="list-disc">
+      <div class="flex flex-col gap-y-3 items-center justify-between">
+        <h1 class="font-semibold underline text-center text-5xl text-gray-800 py-4"> {{ project.title }} </h1>
+        <div>
+          <p v-html="project.description">
+          </p>
+        </div>
+        <button type="submit"
+          class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-indigo-400 hover:bg-indigo-600 rounded-lg">
+          Apply to project
+        </button>
       </div>
-      <button type="submit"
-        class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-indigo-400 hover:bg-indigo-600 rounded-lg">
-        Apply to project
-      </button>
-    </div>
+    </html>
   </GuestLayout>
 </template>
 
@@ -19,6 +20,6 @@
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 
 defineProps({
-    project: Object,
+  project: Object,
 })
 </script>
