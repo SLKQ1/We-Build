e6e336c31f04223db177e34d0b06569fdaf65153
Faiz Qureshi <qureshi.a.faiz@gmail.com>
2022-12-23 17:57:41 -0700

    created relationships on application model for user and project

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -4,15 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Application extends Model
 {
     use HasFactory;
-    
+
     // Application statuses 
-    const PENDING = 0; 
-    const ACCEPTED = 1; 
-    const REJECTED = 2; 
+    const PENDING = 0;
+    const ACCEPTED = 1;
+    const REJECTED = 2;
 
     /**
      * The table associated with the model.
@@ -20,11 +21,27 @@ class Application extends Model
      * @var string
      */
     protected $table = 'applications';
-    
+
     /**
      * The attributes that aren't mass assignable.
      *
      * @var array
      */
     protected $guarded = [];
+
+    /**
+     * Public function to get the user for this application
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Public function to get the project for this application
+     */
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
